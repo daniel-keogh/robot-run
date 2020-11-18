2ca686e3b447fdfda3e04cbe8b9cc5be181e978b
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-11-18 15:58:00 +0000

    Get tiles spawning & destroying automatically

---
+++ Assets/__Scripts/Tiles/Tile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] float destoryDelay = 3f;
+
+    private TileSpawner tileSpawner;
+
+    void Start()
+    {
+        tileSpawner = FindObjectOfType<TileSpawner>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            tileSpawner.SpawnTile();
+            Destroy(gameObject, destoryDelay);
+        }
+    }
+}
---
+++ Assets/__Scripts/Tiles/TileSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSpawner : MonoBehaviour
+{
+    [SerializeField] private Tile tilePrefab;
+
+    private SpawnPoint spawnPoint;
+
+    void Start()
+    {
+        // Get active tiles & set the next spawn point
+        var tiles = GetComponentsInChildren<Tile>();
+        spawnPoint = tiles[tiles.Length - 1].GetComponentInChildren<SpawnPoint>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void SpawnTile()
+    {
+        var tile = Instantiate(tilePrefab, spawnPoint.transform.position, Quaternion.identity);
+        tile.transform.SetParent(transform);
+
+        // Set current tile as next spawn point
+        spawnPoint = tile.GetComponentInChildren<SpawnPoint>();
+    }
+}
