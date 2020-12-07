e29ad85221220e6c1f3b11420b8a04c2fe414d4d
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-12-07 23:33:31 +0000

    Add some music & sfx

---
+++ Assets/__Scripts/Controllers/MusicController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicController : SingletonController
+{
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void ToggleMusic()
+    {
+        audioSource.mute = !audioSource.mute;
+    }
+
+    public bool IsMuted()
+    {
+        return audioSource.mute;
+    }
+}
